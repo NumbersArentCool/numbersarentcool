a8648d645f7b6983f0d4cd0b08f4837f41c3bae7
Wedrig <jackkmorice@gmail.com>
2016-12-21 09:02:16 -0600

    Update number.cpp

--- number.cpp
+++ number.cpp
@@ -1,1 +1,21 @@
+#include <iostream.h>
+#include <string
+using namespace std 
 
+int main ()
+{
+  string userInp = "";
+  cout << "While i'm gone, don't put in any numbers, or try to crack any codes";
+}
+
+int #1 ()
+{
+  string userInp = "1";
+  cout << "We are number one!";
+}
+
+int 420()
+{
+  string userInp = "420";
+  cout << "drugs aren't cool";
+}
